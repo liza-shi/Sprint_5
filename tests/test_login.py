53db90511f7903f4a8124f639b7e4dbded1675dc
Elizaveta Shishlo <lizakurtsevich@yandex.com>
2026-07-30 23:25:40 +0300

    step before the check

--- tests/test_login.py
+++ tests/test_login.py
@@ -3,9 +3,6 @@ from helpers import (click, login_from_open_form, register_unique_user, wait_vis
 from locators import Locators
 
 
-def assert_login_successful(driver):
-    assert (driver.current_url.rstrip("/") == Urls.BASE_URL.rstrip("/"))
-    assert wait_visible(driver, Locators.ORDER_BUTTON).is_displayed()
 
 class TestLogin:
     def test_login_by_login_account_button_on_main_page(self, driver):
@@ -14,7 +11,8 @@ class TestLogin:
         click(driver, Locators.LOGIN_ACCOUNT_BUTTON)
         login_from_open_form(driver, user)
 
-        assert_login_successful(driver)
+        assert (driver.current_url.rstrip("/") == Urls.BASE_URL.rstrip("/"))
+        assert wait_visible(driver, Locators.ORDER_BUTTON).is_displayed()
 
 
     def test_login_by_personal_account_link(self, driver):
@@ -23,7 +21,8 @@ class TestLogin:
         click(driver, Locators.PERSONAL_ACCOUNT_LINK)
         login_from_open_form(driver, user)
 
-        assert_login_successful(driver)
+        assert (driver.current_url.rstrip("/") == Urls.BASE_URL.rstrip("/"))
+        assert wait_visible( driver, Locators.ORDER_BUTTON).is_displayed()
 
 
     def test_login_by_link_in_registration_form(self, driver):
@@ -32,7 +31,8 @@ class TestLogin:
         click(driver, Locators.LOGIN_LINK_IN_FORM)
         login_from_open_form(driver, user)
 
-        assert_login_successful(driver)
+        assert (driver.current_url.rstrip("/") == Urls.BASE_URL.rstrip("/"))
+        assert wait_visible(driver, Locators.ORDER_BUTTON).is_displayed()
 
 
     def test_login_by_link_in_password_recovery_form(self, driver):
@@ -41,5 +41,5 @@ class TestLogin:
         click(driver, Locators.LOGIN_LINK_IN_FORM)
         login_from_open_form(driver, user)
 
-        assert_login_successful(driver)
-
+        assert (driver.current_url.rstrip("/") == Urls.BASE_URL.rstrip("/"))
+        assert wait_visible(driver, Locators.ORDER_BUTTON).is_displayed()
